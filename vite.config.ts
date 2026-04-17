be9062d20e8beb71356bbe4e909de190fe659838
Manikumar Malle <mani.malle.dev@gmail.com>
2026-04-17 16:15:59 +0530

    second cat commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,12 @@ import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 import babel from '@rolldown/plugin-babel';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] }),
-    tailwindcss(),
-  ],
+base: "/cat-repo/",   // ✅ IMPORTANT
+
+plugins: [
+react(),
+babel({ presets: [reactCompilerPreset()] }),
+tailwindcss(),
+],
 });
